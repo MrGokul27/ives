ca1c06b5d1e11b4b97ef4ec446ccd312e9c7a8a2
gokulanath <gokulanath.p@hepl.com>
2024-08-27 14:36:26 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Shop from './pages/Shop';
-import ShopMen from './pages/ShopMen';
-import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Contact from './pages/Contact';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import ShopMen from "./pages/ShopMen";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Contact from "./pages/Contact";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/shop" element={<Shop/>} />
-        <Route path="/shop-men" element={<ShopMen/>} />
-        <Route path="/product/:id" element={<ProductDetail/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/checkout" element={<Checkout/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop-men" element={<ShopMen />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
